3906866a9b2a5206793ca07eb1171ee49abc343d
MKoushikyadav <46778240+sonicsplasher@users.noreply.github.com>
2023-08-12 00:29:09 +0530

    Added the help command and removed a glitch with clear not removing help command.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+//Text Replies for a few commands:
+const help =
+"\
+<b>Welcome to Koushik's CLI</b><br>\
+For more help on a command, type 'command name' help.<br>\
+ls - Display the available applications<br>\
+run - Run the application in either modern or CLIish look<br>\
+clear - clear the terminal<br>\
+clearscreen - same as 'clear'<br>\
+cls - same as 'clear'\
+"
+
+
 
 // This function initializes the input box i.e the command line.
 function getcmd() 
@@ -15,35 +28,45 @@ function processCommand(event){
     }   
 }
 
-// This function logs the commands on screen i.e creates an paragraph element with class cmdAreaEntered and is inserted before the flex div i.e
-// the text input. 
-function logCommand(text) {
+/* This function logs the commands on screen i.e creates an paragraph element with class cmdAreaEntered and is inserted before the flex div i.e
+ the text input. */
+function logCommand(text,prefix='>') {
     let entered = document.createElement('p');
     entered.classList.add('cmdAreaEntered');
-    entered.textContent = ' > ' + text;
+    entered.textContent = prefix + text;
     document.getElementById('flex').before(entered);
     cmd.value = '';
     cmd.placeholder = '';
 }
 
-/**
-@param {string} text
-@return {string}
- */
+
+// This function clears the screen that is removes all nodes of class cmdAreaEntered.
+function clear() {elements=document.getElementsByClassName('cmdAreaEntered');
+        [...elements].forEach((element) => {
+            element.remove();
+        });}
+
 
 //This function processes the commands by listening to the input.
 function runCommand(text){
     switch (text) {
         // if cls,clear or clearscreen is entered ,it removes all the commands entered from the screen.
         case "cls": case "clear": case "clearscreen":
-        elements=document.getElementsByClassName('cmdAreaEntered');
-        [...elements].forEach((element) => {
-            element.remove();
-        });
+        
+        clear();
         break;
         // if help is entered, a list of commands that can be entered are shown.
         case "help":
-
+            let entered = document.createElement('p');
+            entered.classList.add('cmdAreaEntered');
+            entered.innerHTML = help;
+            document.getElementById('flex').before(entered);
+            cmd.value = '';
+            cmd.placeholder = '';
+            break;
+        case "ls":
+            
+            break;
         default:
         break;
     }
